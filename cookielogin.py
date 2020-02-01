9ed4da3643e087d2b7efc3a5a9ddf261880e38d1
reckontt <46804217+reckontt@users.noreply.github.com>
2020-02-01 14:17:21 +0800

    py file
    login cookies

---
+++ cookielogin.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+dr=webdriver.Chrome()
+dr.get('http://www.baidu.com')
+#添加百度登录的cookie
+dr.add_cookie({'name':'BDUSS','value':'Fva1NhbVc1dUF0TDRmQmdXOUkxNzdBQktuZE5scVVBS1IzT3VMfm9YS0FsbTVjQVFBQUFBJCQAAAAAAAAAAAEAAAB-jVkjyOe46MqrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAJR1yACUdcaj'})
+#刷新网页
+dr.refresh()
