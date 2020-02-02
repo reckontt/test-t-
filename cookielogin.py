bb550fca08a35422bbe254a73ba9d6705afc8a89
reckontt <ttx@glasssix.com>
2020-02-02 12:07:38 +0800

    第N次测试修改

--- cookielogin.py
+++ cookielogin.py
@@ -4,4 +4,5 @@ dr.get('http://www.baidu.com')
 #添加百度登录的cookie
 dr.add_cookie({'name':'BDUSS','value':'Fva1NhbVc1dUF0TDRmQmdXOUkxNzdBQktuZE5scVVBS1IzT3VMfm9YS0FsbTVjQVFBQUFBJCQAAAAAAAAAAAEAAAB-jVkjyOe46MqrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAIAJR1yACUdcaj'})
 #刷新网页
-dr.refresh()
+test
+r.refresh()
